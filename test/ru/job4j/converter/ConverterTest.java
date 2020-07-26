1cff9f50ce095a429464ec407e5c704c955afdd1
maD-skillz <warneverchang3s@gmail.com>
2020-07-26 13:26:25 +0300

    1. Модульные тесты. [#331554]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -13,9 +13,8 @@ public class ConverterTest {
         int in = 140;
         int expected = 2;
         int out = Converter.rubleToEuro(in);
-        boolean passed = expected == out;
         Assert.assertEquals(expected, out);
-        System.out.println("140 rubles are 2 Test result : " + passed);
+        System.out.println("140 rubles are 2 Test result : " + out);
 
     }
 
@@ -24,8 +23,7 @@ public class ConverterTest {
         int in = 180;
         int expected = 3;
         int out = Converter.rubleToDollar(in);
-        boolean passed = expected == out;
         Assert.assertEquals(expected, out);
-        System.out.println("180 rubles are 3 Test result : " + passed);
+        System.out.println("180 rubles are 3 Test result : " + out);
     }
 }
