837c968b07161dfba0921dd4cd7810ebe34549fa
maD-skillz <warneverchang3s@gmail.com>
2020-08-21 15:07:22 +0300

    6.0.2. Заполнение массива. [#331500]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Egor Popov";
+        names[2] = "Jason Statham";
+        names[3] = "Vin Diesel";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
